abd58840d1b19f7480717ff58709f1ff25bfae10
imayuxiang <losywar@163.com>
2015-08-16 12:30:05 +0800

    first file

---
+++ test.cpp
@@ -0,0 +1,21 @@
+/**
+*(C) 2010-2012 Alibaba Group Holding Limited.
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* version 2 as published by the Free Software Foundation.
+*
+*  > Created Time: Sun Aug 16 12:25:15 2015
+*
+*  > File Name: test.cpp
+*
+*  > Mail: yuxiangma@alibaba-inc.com
+*
+*  > Authors: Zi Chen
+*
+*/
+#include<iostream>
+using namespace std;
+
+int main() {
+  cout<<"hello github!"<<endl;
+}
